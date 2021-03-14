1160d75290476bc477cd05e1dd1dac66a6e57b21
Matthew Brown <mbrown@geomarvel.com>
2021-03-14 18:51:46 -0400

    created theme

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -16,7 +16,7 @@ import { Searchbar } from "react-native-paper";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight};
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
 `;
 
 const SearchContainer = styled.View`
